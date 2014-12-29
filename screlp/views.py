70400b38263ad7ad0be960363df1bc5d3040d927
Jeffrey D <literallyelvis@gmail.com>
2014-12-28 21:32:23 -0600

    Everything is working-ish.

--- screlp/views.py
+++ screlp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-import backend.geog as geog
-import backend.parse as parse
+from django.conf import settings
+from screlp.backend import parse, geog, connect
 import time
 
 
@@ -22,21 +22,19 @@ def result(request):
     args["density"] = request.GET["d"]
     args["category"] = request.GET["c"]
 
-    map_url = "http://maps.googleapis.com/maps/api/staticmap?zoom=12&size=500x500&scale=2&maptype=roadmap"
-    map_url += "&center={0}".format(args["address"].replace(" ", "+"))
-
     if "visited" in request.session:
         creds = [request.session["con_key"],
                  request.session["con_secret"],
                  request.session["token"],
                  request.session["token_secret"]]
     else:
-        creds = [request.GET["con_key"], request.GET["con_secret"], request.GET["token"], request.GET["token_secret"]]
+        creds = settings.YELP_CREDENTIALS
 
     start = time.time()
     origin = geog.get_geocode(args)
     coords = geog.generate_coords(origin, int(args["density"]), int(args["radius"]))
     yelp_results = parse.scrape_yelp(args, coords, creds)
+    """
     if not yelp_results:
         return render(request, "error.html")
     if "visited" not in request.session:
@@ -45,16 +43,15 @@ def result(request):
         request.session["con_secret"] = request.GET["con_secret"]
         request.session["token"] = request.GET["token"]
         request.session["token_secret"] = request.GET["token_secret"]
+    """
     time_taken = "Execution time: {:.2f}{}".format((time.time() - start), " seconds")
 
     orglat, orglong = origin
-    map_url += "&markers=color:blue%7Clabel:X%7C{0},{1}".format(orglat, orglong)
-
-    for lat, long in coords:
-        map_url += "&markers=color:red%7Clabel:%7C{0},{1}".format(lat, long)
 
     return render(request, "result.html", {"args": args,
-                                           "map_url": map_url,
+                                           "coords": coords,
+                                           "lat": orglat,
+                                           "long": orglong,
                                            "yelp_results": yelp_results,
                                            "exec_time": time_taken})
 
@@ -62,4 +59,4 @@ def result(request):
 def clear(request):
     request.session.flush()
     return render(request, "redirect.html")
-    
+
