5873bfce5467de75b48586767a906e770b3e6d98
Jeffrey D <literallyelvis@gmail.com>
2015-01-08 23:00:15 -0600

    Added login and registration forms.

---
+++ screlp/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    user_email = forms.EmailField
+    password = forms.CharField(widget=forms.PasswordInput())
+
+
+class RegistrationForm(forms.Form):
+    user_email = forms.EmailField
+    password = forms.CharField(widget=forms.PasswordInput())
+	password_verify = forms.CharField(widget=forms.PasswordInput())
