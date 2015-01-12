5296e45275d6c8f37c2ebbb353ecc82045568451
Jeffrey D <literallyelvis@gmail.com>
2015-01-12 01:57:22 -0500

    Improved forms styling and basework for registration handling.

--- screlp/forms.py
+++ screlp/forms.py
@@ -2,11 +2,11 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    user_email = forms.EmailField
-    password = forms.CharField(widget=forms.PasswordInput())
+    user_email = forms.EmailField(widget=forms.TextInput(attrs={'class':'parameters'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'parameters'}))
 
 
 class RegistrationForm(forms.Form):
-    user_email = forms.EmailField
-    password = forms.CharField(widget=forms.PasswordInput())
-    password_verify = forms.CharField(widget=forms.PasswordInput())
+    user_email = forms.EmailField(widget=forms.TextInput(attrs={'class':'parameters'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'parameters'}))
+    password_verify = forms.CharField(widget=forms.PasswordInput(attrs={'class':'parameters'}))
