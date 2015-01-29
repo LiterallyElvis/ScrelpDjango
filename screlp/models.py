6e06b51532f46bf40c66655dda97103742f3b3ba
Jeffrey D <literallyelvis@gmail.com>
2015-01-29 01:26:54 -0600

    Rough sketch of models

--- screlp/models.py
+++ screlp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    yelp_name = models.CharField(max_length=200)
+    human_name = models.CharField(max_length=200)
+
+
+class YelpCredentials(models.Model):
+    consumer_key = models.CharField(max_length=200)
+    consumer_secret = models.CharField(max_length=200)
+    token = models.CharField(max_length=200)
+    token_secret = models.CharField(max_length=200)
+
+
+class GoogleMapsCredentials(models.Model):
+    api_key = models.CharField(max_length=200)
